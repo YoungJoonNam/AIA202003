13245fc5fa6dc0c60d7ad8db231b6f82936d7e57
YoungJoonNam <njoonk@gmail.com>
2020-04-09 10:50:46 +0900

    Update Test2.java
    println print 테스트ㅜ

--- Exam/src/Test2.java
+++ Exam/src/Test2.java
@@ -7,16 +7,14 @@
 public class Test2 {
 	public static void main(String[] args) {
 		
-		System.out.println(15);
+		System.out.print(15);
 		System.out.println("1"+5);
 		System.out.println("15");
 		System.out.println(10+5);
 		System.out.println(1+"5");
 		System.out.println("1"+"5");
 		
-		
-		
-		
+				
 
 	}
 }
