94f14b3068cc561ee580cae5acba83a7c2a2ab59
YoungJoonNam <njoonk@gmail.com>
2020-04-09 15:48:54 +0900

    Create BooleanVar.java
    실습

---
+++ Exam/src/BooleanVar.java
@@ -0,0 +1,27 @@
+
+public class BooleanVar {
+
+	public static void main(String[] args) {
+
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(3<4); // >,<,==,>=,<=
+		System.out.println(3>4);
+		//System.out.println("Is True",3>4);
+		
+		int num = 100;
+		
+		
+		float number = 0.1f;
+		
+		int ntest = 2147483647;
+		long ltest = 2147483648L;
+		
+		System.out.println(num>4);
+
+	}
+
+}
