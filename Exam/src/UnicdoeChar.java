71ce8d5beb17f08cf337114511e06d182be0c445
YoungJoonNam <njoonk@gmail.com>
2020-04-09 15:49:00 +0900

    Create UnicdoeChar.java
    실습

---
+++ Exam/src/UnicdoeChar.java
@@ -0,0 +1,16 @@
+
+public class UnicdoeChar {
+
+	public static void main(String[] args) {
+		
+		char ch1 = 'A';
+		char ch2 = '한';
+		char ch3 = 67;
+		System.out.println(ch1);
+		System.out.println(ch2);
+		System.out.println(ch3);
+		
+		
+	}
+
+}
