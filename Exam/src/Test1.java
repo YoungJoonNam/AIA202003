aaa2d2687df29fa6df2edc33316ed0cca00c1d2f
YoungJoonNam <njoonk@gmail.com>
2020-04-09 18:21:48 +0900

    Update Test1.java
    실습

--- Exam/src/Test1.java
+++ Exam/src/Test1.java
@@ -9,9 +9,28 @@ public class Test1 {
 		 System.out.println( “2+5=” + (2+5) );
 		*/
 		
-		System.out.println("2+5=" + 2 + 5);
-		System.out.println("2+5=" + (2 + 5));
+		int If=0; 
+		//byte b = 10;
+		char ch = 'A';
+		int i = 100;
+		//long l = 1000L;
+		
+		 //b = (byte)i;
+		//ch = (char)b;
+		//short s =ch;
+		//float f = l;
+		i = (int)ch;
+		
+//		System.out.println("2+5=" + 2 + 5);
+//		System.out.println("2+5=" + (2 + 5));
 
+//		System.out.println("1" + "2");
+		System.out.println(null);
+//		System.out.println('A'+ 'B');
+//		System.out.println('1'+ 2);
+//		System.out.println('1'+ '2');
+//		System.out.println('J'+ "ava");
+//		System.out.println(true+ null);
 	}
 
 }
