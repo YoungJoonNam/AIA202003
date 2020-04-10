25656b1d22359165ebfd29fed774b59e8a089fd5
YoungJoonNam <njoonk@gmail.com>
2020-04-10 20:16:31 +0900

    Create PostfixOp.java
    실습

---
+++ Day20200410/src/ch_operator_/PostfixOp.java
@@ -0,0 +1,19 @@
+package ch_operator_;
+
+public class PostfixOp {
+
+	public static void main(String[] args) {
+		
+		int nNum1 = 7;				
+		int nNum2 = nNum1++;		
+		int nNum3 = nNum1--;
+		
+		System.out.println(nNum1++); 
+		System.out.println(nNum2);
+		System.out.println(nNum2--);
+		System.out.println(nNum3);
+		
+
+	}
+
+}
