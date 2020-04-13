e113b197f8f51469a2c660fd61273325916cad26
YoungJoonNam <njoonk@gmail.com>
2020-04-13 17:03:07 +0900

    Create WhileBasic.java
    20200413 예제

---
+++ Day20200413/src/conditionalandloop/WhileBasic.java
@@ -0,0 +1,18 @@
+package conditionalandloop;
+
+public class WhileBasic {
+
+	public static void main(String[] args) {
+
+
+		int num=0; // 반복의 조건을 체크하는 변수
+		
+		while (num<5) {
+			System.out.println("I like JAVA" + num);
+			num++;
+			
+		}
+
+	}
+
+}
