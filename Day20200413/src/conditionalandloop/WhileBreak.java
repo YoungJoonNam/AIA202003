3cdc70e0290174b550680d92c8bfddeefb5983d7
YoungJoonNam <njoonk@gmail.com>
2020-04-13 17:03:10 +0900

    Create WhileBreak.java
    20200413 예제

---
+++ Day20200413/src/conditionalandloop/WhileBreak.java
@@ -0,0 +1,29 @@
+package conditionalandloop;
+
+public class WhileBreak {
+
+	public static void main(String[] args) {
+
+		
+		int i = 0;
+		int sum = 0;
+
+		// 무한 반복
+		while (true) {
+			
+			if(sum>100) {
+				break;				
+			}
+			
+			System.out.println(0);
+			i++;
+			sum += i;			
+		}
+		
+		System.out.println("마지막 값 : " + i);
+		System.out.println("합이 100을 넘어간 값 : " + sum);
+			
+
+	}
+
+}
