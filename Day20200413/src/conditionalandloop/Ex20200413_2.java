3a4ce351109c11b30547d92a5ca3d745c8f0718a
YoungJoonNam <njoonk@gmail.com>
2020-04-14 18:24:44 +0900

    Update Ex20200413_2.java
    실습

--- Day20200413/src/conditionalandloop/Ex20200413_2.java
+++ Day20200413/src/conditionalandloop/Ex20200413_2.java
@@ -8,88 +8,113 @@ public class Ex20200413_2 {
 //		 
 //
 //		1. int형 변수 x가 10보다 크고 20보다 작을 때 true인 조건식
-//
-//		 
-//
-//		 
-//
-//		 
+		
+		System.out.println("\n[문제1]-----------------\n");
+		int x=17;
+		
+		if(x>10 && x<20) {
+			System.out.println("x는 10보다 크고 20보다 작습니다. x는 "+x);
+		}
+		
+				 
 //
 //		2. char형 변수 ch가 공백이나 탭이 아닐 때 true인 조건식
 //
-//		 
-//
-//		 
-//
-//		 
+		System.out.println("\n[문제2]-----------------\n");
+		
+		char ch;
+		ch='A';
+		
+		if (ch!=' ' && ch!='\t') {
+			System.out.println("ch는 공백이나 탭이 아닙니다. ch는 "+ch);
+		}
 //
 //		3. char형 변수 ch가 ‘x' 또는 ’X'일 때 true인 조건식
 //
-//		 
-//
-//		 
-//
-//		 
+		System.out.println("\n[문제3]-----------------\n");
+		
+		ch='X';
+		
+		if(ch=='x' || ch=='X') {
+			System.out.println("ch는 x 혹은 X 입니다. ch는 "+ch);
+		}
+ 
 //
 //		4. char형 변수 ch가 숫자(‘0’~‘9’)일 때 true인 조건식
 //
-//		 
-//
-//		 
-//
-//		 
+		
+		System.out.println("\n[문제4]-----------------\n");
+		
+		ch = '7';
+		if(ch>='0' && ch<='9') {
+			System.out.println("ch는 숫자(‘0’~‘9’) 입니다. ch는 "+ch);
+		}
 //
 //		5. char형 변수 ch가 영문자(대문자 또는 소문자)일 때 true인 조건식
 //
-//		 
-//
-//		 
-//
-//		 
-//
+		System.out.println("\n[문제5]-----------------\n");
+		ch = 'D';
+		if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')) {
+			System.out.println("ch는 ch가 영문자(대문자 또는 소문자). ch는 "+ch);
+		}//
 //		6. int형 변수 year가 400으로 나눠떨어지거나 또는 4로 나눠떨어지고 100으로나눠떨어지지않을 때 true인 조건식
 //
-//		 
-//
-//		 
-//
-//		 
+		System.out.println("\n[문제6]-----------------\n");
+		
+		int year=132;
+		
+		if((year%400 == 0) || ((year%4 == 0) && (year%100 != 0))){
+			System.out.println("400으로 나눠떨어지거나 또는 4로 나눠떨어지고 100으로나눠떨어지지않음 year는 "+year);
+		}
 //
 //		7. boolean형 변수 powerOn가 false일 때 true인 조건식
 //
-//		 
-//
-//		 
-//
-//		 
+		System.out.println("\n[문제7]-----------------\n");
+		
+		boolean powerOn = false;
+		
+		if(!powerOn) {
+			System.out.println("powerOn은 false 입니다"+powerOn);
+		}
 //
 //		8. 문자열 참조변수 str이 “yes”일 때 true인 조건식
-//
-//		 
-//
-//		 
-//
-//		 
-//
-//		 
-//
-//		 
-//
-//		 
+		
+		System.out.println("\n[문제8]-----------------\n");
+		
+		String str="yes";
+		if(str == "yes") {
+			System.out.println("str 값은 " + str);
+		}
 //
 //		[2] 1부터 20까지의 정수 중에서 2 또는 3의 배수가 아닌 수의 총합을 구하시오
 //
-//		 
-//
-//		 
-//
-//		 
-//
+		System.out.println("\n[문제[2]]-----------------\n");
+		
+		int num2=1,sum2=0;
+		while (num2<=20) {
+			
+			if((num2%2 !=0) && (num2%3 !=0)){
+				sum2 += num2;				
+				System.out.println(">>"+num2+" "+sum2);
+			}
+			num2++;					
+		}
+		
+		System.out.println("1부터 20까지의 정수 중에서 2 또는 3의 배수가 아닌 수의 총합: "+sum2);
+
 //		[3] 1+(1+2)+(1+2+3)+(1+2+3+4)+...+(1+2+3+...+10)의 결과를 계산하시오.
 //
-//		 
+		System.out.println("\n[문제[3]]-----------------\n");		 
 //
-//		 
+		int nsum3=0;
+		for (int i = 1; i <= 10; i++) {
+			
+			nsum3 = nsum3 + i*(11-i);	
+			
+		}
+		System.out.println("RESULT>>" + nsum3);
+		nsum3 = 1*10+2*9+3*8+4*7+5*6+6*5+7*4+8*3+9*2+10*1;
+		System.out.println(nsum3);
 //
 //		 
 //
