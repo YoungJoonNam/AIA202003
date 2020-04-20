cef694247a972c92a97232d3a23e59e1acc62980
YoungJoonNam <njoonk@gmail.com>
2020-04-20 16:50:44 +0900

    Create PhoneInfor.java
    과제

---
+++ Day20200420/src/classandinstance/PhoneInfor.java
@@ -0,0 +1,22 @@
+package classandinstance;
+
+public class PhoneInfor {
+	
+//	전화번호 저장
+//	전화번호 정보 뷰
+//	전화번호 검색
+//	전화번호 삭제
+
+
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
