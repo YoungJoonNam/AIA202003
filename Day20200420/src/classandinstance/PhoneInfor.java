e5c7c2a524097284afd4fde447a0ff504b8ac993
YoungJoonNam <njoonk@gmail.com>
2020-04-21 11:54:51 +0900

    Update PhoneInfor.java
    버전 0.3 완료

--- Day20200420/src/classandinstance/PhoneInfor.java
+++ Day20200420/src/classandinstance/PhoneInfor.java
@@ -85,9 +85,6 @@ public class PhoneInfor {
 		return bResult;
 	}
 	
-	
-	
-
 	public static void main(String[] args) {
 
 		Scanner scInputUser=new Scanner(System.in);
@@ -97,6 +94,8 @@ public class PhoneInfor {
 		String strInputBirthday=null;
 			
 		PhoneInfor[] PhoneBook = new PhoneInfor[100];
+		boolean bSearchSuccess = false;
+		int nIndex=0;
 		
 		
 		while(true) {
@@ -105,11 +104,11 @@ public class PhoneInfor {
 			System.out.println("--------------------------------------------------------------------");
 			System.out.println("[전화번호부]");
 			System.out.println("--------------------------------------------------------------------");
-			System.out.println("<메뉴>-------( ^O^)/");
+			System.out.println("○메뉴");
 			System.out.println("         - 입력,저장 : I 혹은 i");
-			System.out.println("         - 검색 : F 혹은 f  //미구현");
-			System.out.println("         - 삭제 : D 혹은 d //미구현");
-			System.out.println("         - 종료는 X 혹은 x");
+			System.out.println("         - 검색 : F 혹은 f");
+			System.out.println("         - 삭제 : D 혹은 d");
+			System.out.println("         - 종료 : X 혹은 x");
 			//저장 : 이름, 전화번호, 생년월일 정보를 대상으로 하는 저장
 			//검색 : 이름을 기준으로 데이터를 찾아서 해당 데이터의 정보를 출력
 			//삭제 : 이름을 기준으로 데이터를 찾아서 해당 데이터를 삭제
@@ -118,16 +117,16 @@ public class PhoneInfor {
 			if(strInputMenu.contentEquals("X")||strInputMenu.contentEquals("x")) {
 				System.out.println("감사합니다. 전화번호부를 종료합니다~~");
 				break;				
-			}
+			}//if(strInputMenu.contentEquals("X")||strInputMenu.contentEquals("x")) {
 			else if(strInputMenu.contentEquals("I")||strInputMenu.contentEquals("i")) {
 			
-				System.out.println("이름을 입력해주세요 : ");
+				System.out.print("이름을 입력해주세요 : ");
 				strInputName=scInputUser.nextLine();
 				
-				System.out.println("전화번호를 입력해주세요 : ");
+				System.out.print("전화번호를 입력해주세요 : ");
 				strInputNumber=scInputUser.nextLine();
 				
-				System.out.println("생년월일을 입력해주세요 : ");
+				System.out.print("생년월일을 입력해주세요 : ");
 				strInputBirthday=scInputUser.nextLine();
 				
 				PhoneInfor pi;
@@ -139,37 +138,115 @@ public class PhoneInfor {
 					pi = new PhoneInfor(strInputName,strInputNumber,strInputBirthday);					
 				}
 				
-				System.out.println("[사용자 입력정보]");
+				System.out.println("\n[사용자 입력정보]");
 				pi.DoPrintPhoneBook();		
 				
 				System.out.println("저장하시겠습니까??? (Y or N)");
 				strInputMenu=scInputUser.nextLine();
 				if(strInputMenu.contentEquals("Y")||strInputMenu.contentEquals("y")) {
-					//이름이 비어 있는 저장공간을 찾아서 저장
-					PhoneBook[0] = pi;
-					System.out.println("[저장 완료]");
+					//앞에서부터 순차적으로 검색, 이름이 비어 있는 저장공간을 찾아서 저장
+					//추가 : 이름이 동일한 사람이 있는 경우 입력하지 않도록
+					bSearchSuccess = false;
+					for (nIndex = 0; nIndex < PhoneBook.length; nIndex++) {
+						if(PhoneBook[nIndex] == null) {
+							PhoneBook[nIndex] = pi;
+							bSearchSuccess = true;
+							break;
+						}						
+					}
+					
+					if(bSearchSuccess) {
+						System.out.println("[저장 완료] - Index : "+nIndex);						
+					}
+					else {
+						System.out.println("!! 저장 실패 - 빈공간이 없습니다.");
+					}
+					
 				}
 				else {
 					System.out.println("[저장 취소]");
 					
 				}
 				
-			} // 사용자 입력 부분
-			else if(strInputMenu.contentEquals("F")||strInputMenu.contentEquals("f")) {
-				// 검색 후 출력
-			}
-			else if(strInputMenu.contentEquals("D")||strInputMenu.contentEquals("d")) {
+				System.out.println("-계속하시려면 아무키나 눌러주세요");
+				strInputMenu=scInputUser.nextLine();
 				
-			}
+			} // else if(strInputMenu.contentEquals("I")||strInputMenu.contentEquals("i")) {
+			else if(strInputMenu.contentEquals("F")||strInputMenu.contentEquals("f")) {
+				//검색 : 이름을 기준으로 데이터를 찾아서 해당 데이터의 정보를 출력
+				
+				System.out.print("검색할 이름을 입력해주세요 : ");
+				strInputMenu=scInputUser.nextLine();				
+				bSearchSuccess = false;
+				for (nIndex = 0; nIndex < PhoneBook.length; nIndex++) {
+					if(PhoneBook[nIndex] != null) {
+						if(strInputMenu.contentEquals(PhoneBook[nIndex].strPname)) {
+							System.out.println("찾았습니다!! Index : " + nIndex);
+							PhoneBook[nIndex].DoPrintPhoneBook();		
+							bSearchSuccess = true;
+							break;
+						}					
+					}												
+				}				
+				
+				if(!bSearchSuccess) {
+					System.out.println("-입력하신 이름에 해당하는 정보가 없습니다.");					
+				}
+				
+				System.out.println("-계속하시려면 아무키나 눌러주세요");
+				strInputMenu=scInputUser.nextLine();
+			}//else if(strInputMenu.contentEquals("F")||strInputMenu.contentEquals("f")) {
+			else if(strInputMenu.contentEquals("D")||strInputMenu.contentEquals("d")) {
+				//삭제 : 이름을 기준으로 데이터를 찾아서 해당 데이터를 삭제
+				//데이터 삭제 후 남아있는 데이터 처리는 데이터를 빈 공란이 없이 순차적으로
+				//재정리 2번이 삭제되었다면 3번 이후 데이터들의 주소 값이 -1 처리되어 재저장.
+				
+				System.out.print("삭제할 이름을 입력해주세요 : ");
+				strInputMenu=scInputUser.nextLine();				
+				bSearchSuccess = false;
+				for (nIndex = 0; nIndex < PhoneBook.length; nIndex++) {
+					if(PhoneBook[nIndex] != null) {
+						if(strInputMenu.contentEquals(PhoneBook[nIndex].strPname)) {
+							System.out.println("찾았습니다!! Index : " + nIndex);
+							PhoneBook[nIndex].DoPrintPhoneBook();		
+							bSearchSuccess = true;
+							break;
+						}					
+					}												
+				}				
+				
+				if(!bSearchSuccess) {
+					System.out.println("-입력하신 이름에 해당하는 정보가 없습니다.");					
+				}
+				else {
+					System.out.println("정말 삭제하시겠습니까?? (Y/N)");
+					strInputMenu=scInputUser.nextLine();					
+					if(strInputMenu.contentEquals("Y")||strInputMenu.contentEquals("y")) {
+						PhoneBook[nIndex] = null;
+						System.out.println("삭제하였습니다.");
+					}
+					//삭제된 값 뒤의 값들을 한칸씩 당김
+					//맨끝에 있는 값을 삭제할경우엔 당기지 않음
+					//index 98, 99 삭제시 생각해보기
+					if(nIndex >=  (PhoneBook.length-1)) {
+						
+					}
+					else {
+						for (int i = nIndex; i < PhoneBook.length-1; i++) {
+							PhoneBook[i] = PhoneBook[i+1];						
+						}						
+					}
+					
+					
+				}
+				
+				System.out.println("-계속하시려면 아무키나 눌러주세요");
+				strInputMenu=scInputUser.nextLine();
+				
+			}//else if(strInputMenu.contentEquals("D")||strInputMenu.contentEquals("d")) {
 			else {
 				
 			}
-			
-			
-			
-		}
-		
-
-	}
-
-}
+		} //while(true) {
+	} //public static void main(String[] args) {
+} //public class PhoneInfor {
