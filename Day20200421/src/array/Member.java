d4fbad83deb5d30d08ffba456a688340af0961b1
YoungJoonNam <njoonk@gmail.com>
2020-04-21 18:51:02 +0900

    Update Member.java
    실습

--- Day20200421/src/array/Member.java
+++ Day20200421/src/array/Member.java
@@ -3,6 +3,7 @@ package array;
 public class Member {
 	
 	public String memberId;
+	
 	String memberName;
 	String memberEmail;
 	
@@ -10,5 +11,30 @@ public class Member {
 	public Member(){
 		
 	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getMemberName() {
+		return memberName;
+	}
+
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	
 
 }
