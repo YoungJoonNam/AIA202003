721833132f28042c1637139e6e82732d40496d3b
YoungJoonNam <njoonk@gmail.com>
2020-04-21 18:50:49 +0900

    Create PhoneInfo.java
    과제

---
+++ Day20200421/src/modifier/PhoneInfo.java
@@ -0,0 +1,39 @@
+package modifier;
+
+public class PhoneInfo {
+
+	String strPname;
+	String strPphoneNumber;
+	String strPbirthday;
+
+	{
+		strPname = "";
+		strPphoneNumber = "";
+		strPbirthday = "";
+	}
+	
+	PhoneInfo(){
+		
+	}
+	
+	PhoneInfo(String name, String number, String birthday){
+		strPname = name;
+		strPphoneNumber = number;
+		strPbirthday = birthday;
+	}
+	
+	PhoneInfo(String name, String number){
+		strPname = name;
+		strPphoneNumber = number;		
+		strPbirthday = "[사용자미입력]";
+	}
+	
+	
+	void DoPrintPhoneInfo() {
+			
+		System.out.println("-------------------------------------------------------------------------");
+		System.out.println("이름 - "+strPname+" 전화번호 - "+strPphoneNumber+" 생년월일 - "+strPbirthday);
+		System.out.println("-------------------------------------------------------------------------");
+		
+	}
+}
