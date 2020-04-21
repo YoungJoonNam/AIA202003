362e73747018c19343aa73ded2365cfdb32ae3d8
YoungJoonNam <njoonk@gmail.com>
2020-04-21 16:54:26 +0900

    Create TestMain1.java
    실습

---
+++ Day20200421/src/modifier/TestMain1.java
@@ -0,0 +1,15 @@
+package modifier;
+
+import array.Member;
+
+public class TestMain1 {
+	
+	public static void main(String[] args) {
+
+		Member member = new Member();
+		System.out.println(member.memberId);
+		
+	
+	
+	}
+}
