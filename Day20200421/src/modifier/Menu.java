c13eab38a5345f12521a1d8e9430a938a4a85a7d
YoungJoonNam <njoonk@gmail.com>
2020-04-22 17:10:24 +0900

    Create Menu.java
    메뉴 클래스 분리

---
+++ Day20200421/src/modifier/Menu.java
@@ -0,0 +1,17 @@
+package modifier;
+
+public class Menu {
+	
+	public static void DoPrintMenu() {
+		System.out.println("--------------------------------------------------------------------");
+		System.out.println("[전화번호부]");
+		System.out.println("--------------------------------------------------------------------");
+		System.out.println("○메뉴");
+		System.out.println("         - 입력,저장 : I 혹은 i");
+		System.out.println("         - 검색 : F 혹은 f");
+		System.out.println("         - 삭제 : D 혹은 d");
+		System.out.println("         - 전체목록출력 : P 혹은 p");
+		System.out.println("         - 종료 : X 혹은 x");
+	}
+
+}
