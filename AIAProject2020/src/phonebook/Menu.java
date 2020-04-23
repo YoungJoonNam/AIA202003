f5fdad0588096c5f8aded0244c2553953da8d7bc
YoungJoonNam <njoonk@gmail.com>
2020-04-23 18:31:53 +0900

    Create Menu.java
    프로젝트분리

---
+++ AIAProject2020/src/phonebook/Menu.java
@@ -0,0 +1,17 @@
+package phonebook;
+
+public class Menu {
+	
+	public static void DoPrintMenu() {
+		System.out.println("--------------------------------------------------------------------");
+		System.out.println("[전화번호부]");
+		System.out.println("--------------------------------------------------------------------");
+		System.out.println("○메뉴");
+		System.out.println("         - 1. 입력,저장");
+		System.out.println("         - 2. 검색");
+		System.out.println("         - 3. 삭제");
+		System.out.println("         - 4. 전체목록출력");
+		System.out.println("         - 0. 종료");
+	}
+
+}
